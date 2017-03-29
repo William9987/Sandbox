c31ebcee9fbe20847841798c93631373f49a629c
William9987 <william.lie@my.jcu.edu.au>
2017-03-29 09:47:24 +0800

    remade code

--- oddname.py
+++ oddname.py
@@ -1,8 +1,11 @@
 """
 William
 """
-while True:
-    try:
-        Name = input("Please enter your name = ")
-    except TypeError:
-        print ("Please enter a correct response")
+try:
+    Name = input("Please enter your name = ")
+except TypeError:
+    print ("Please enter a correct response")
+b=""
+for i in range(len(Name)):
+    if (i%2)==0:
+        b+=Name[i]
